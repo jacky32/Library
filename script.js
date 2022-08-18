6fbde36d5244e86ae5fbfb47d6a7445106427521
jacky32 <patrki@atlas.cz>
2022-08-18 21:42:31 +0200

    Create add new book method

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const newBookButton = document.getElementById('new-book-button');
 const newBookModal = document.getElementById('new-book-modal');
 const newBookModalClose = document.getElementsByClassName('new-book-button-close')[0];
 const newBookForm = document.getElementById('new-book');
+const mainContent = document.getElementById('main-content');
+const checkBox = document.getElementById('read');
 
 newBookButton.addEventListener('click', toggleNewBookForm);
 newBookModalClose.addEventListener('click', toggleNewBookForm);
@@ -12,7 +14,7 @@ newBookForm.addEventListener('submit', (e) => {
   const name = newBookForm.elements['name'].value;
   const author = newBookForm.elements['author'].value;
   const pages = newBookForm.elements['pages'].value;
-  const read = newBookForm.elements['read'].value;
+  const read = checkBox.checked;
 
   addBookToLibrary(name, author, pages, read);
   resetNewBookForm();
@@ -43,11 +45,16 @@ function resetNewBookForm() {
 }
 
 function displayBooks() {
-  for (let book in myLibrary) {
-
+  mainContent.innerHTML = '';
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    if (book.read == true) {
+      mainContent.insertAdjacentHTML('afterbegin', `<div class='book read'><div class='book-name'>Book name: ${book.name} </div><div class='book-author'>Book author: ${book.author}</div><div class='book-pages'>Pages: ${book.pages}</div><button class='book-button-read'>Read?</button><button class='book-button-delete'>X</button></></div>`);
+    } else {
+      mainContent.insertAdjacentHTML('afterbegin', `<div class='book'><div class='book-name'>Book name: ${book.name} </div><div class='book-author'>Book author: ${book.author}</div><div class='book-pages'>Pages: ${book.pages}</div><button class='book-button-read'>Read?</button><button class='book-button-delete'>X</button></></div>`);
+    }
   }
 }
-
 
 function toggleNewBookForm() {
   newBookModal.classList.toggle('hidden');
